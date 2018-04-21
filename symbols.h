1e0797a1086a950f745ce473a870898f819a7281
Colin Delorme <re>
2018-04-21 12:21:24 -0400

    rtype?

--- symbols.h
+++ symbols.h
@@ -19,7 +19,7 @@ void Symbols<T>::insert(char* lexeme, T entry)
 }
 
 template <typename T>
-bool Symbols<T>::find(char* lexeme, T& entry)
+bool Symbols<T>::find(char* lexeme, T entry)
 {
 	string name(lexeme);
 	typedef typename map<string, T>::iterator Iterator;
